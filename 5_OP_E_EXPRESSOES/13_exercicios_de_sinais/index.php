f77a2832dd8ba502072106e33152c8fc6aa13b05
FabioSantos99 <120610757+FabioSantos99@users.noreply.github.com>
2023-08-25 20:26:27 -0300

    ok

---
+++ 5_OP_E_EXPRESSOES/13_exercicios_de_sinais/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 5;
+$b = 3;
+
+
+if($a == $b) {
+    echo "A é igual a B <br>";
+}
+
+if($a != $b) {
+    echo "A é diferente a B <br>";
+}
+
+if($a === $b) {
+    echo "A é identico a B <br>";
+}
+
+if($a !== $b) {
+    echo "A é não identico a B <br>";
+}
