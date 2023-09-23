12c1de77f6bfc570e17d7723688182820c4f4a23
FabioSantos99 <120610757+FabioSantos99@users.noreply.github.com>
2023-09-23 01:33:06 -0300

    agenda

---
+++ 17_AGENDA/config/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$host = 'localhost';
+$dbname = 'agenda';
+$user = 'root';
+$pass = "";
+
+
+try {
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+    // Ativar o modo de erros
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+} catch(PDOException $e) {
+    //errro na conexão
+    $error = $e->getMessage();
+    echo "Erro: $error";
+}
---
+++ 17_AGENDA/create.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once("templates/header.php");
+
+?>
+
+<h1>Criando contato</h1>
+<i class="fas fa-eye"></i>
+
+<?php
+
+include_once("templates/footer.php");
+
+?>
