98a13af9e3e48b748d72fa6f0b0260c5f180b70e
FabioSantos99 <120610757+FabioSantos99@users.noreply.github.com>
2023-10-31 20:04:41 -0300

    ok

--- 19_MOVIESTAR/templates/user_review.php
+++ 19_MOVIESTAR/templates/user_review.php
@@ -1,27 +1,23 @@
 <?php
 
+  require_once("models/User.php");
 
-require_once("models/User.php");
+  $userModel = new User();
 
-$userModel = new User();
+  $fullName = $userModel->getFullName($review->user);
 
-$fullName = $userModel->getFullName($review->user);
-
-
-//Checar imagem do usuário
-if($review->user->image == "") {
-  $review->user->image = "user.png";
-}
-
+  // Verifica se tem imagem
+  if(empty($review->user->image)) {
+    $review->user->image = "user.png";
+  }
 ?>
 <div class="col-md-12 review">
   <div class="row">
     <div class="col-md-1">
-      <div id="profile-image-container" class="review-image" style="background-color: url('<?= $BASE_URL ?>img/users/<?= $review->user->image ?>');"></div>
+      <div id="profile-image-container" class="review-image" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $review->user->image ?>')"></div>
     </div>
-
     <div class="col-md-9 author-details-container">
-      <h4 class="author-name"><a href="<?= $BASE_URL ?>profile.php<?= $review->user->id ?>"><?= $fullName ?></a></h4>
+      <h4 class="author-name"><a href="<?= $BASE_URL ?>profile.php?id=<?= $review->user->id ?>"><?= $fullName ?></a></h4>
       <p><i class="fas fa-star"></i> <?= $review->rating ?></p>
     </div>
     <div class="col-md-12">
